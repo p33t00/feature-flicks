303d1dab651d1999f877ccf67d4f5739e9232193
p33t00 <saip8tha@gmail.com>
2023-03-11 23:41:05 +0100

    Error message if Children & Seniors not sync with seats

--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -6,16 +6,14 @@ import Toast from 'react-bootstrap/Toast';
 
 import './Messenger.css'
 
-function Messenger({showMsg}) {
-  const [show, setShow] = useState(false);
+function Messenger({showMsg, setShowMsg, message}) {
+  // const [show, setShow] = useState(false);
 
   return (
     <Row className="messenger">
-      <Col xs={6}>
-        <Toast onClose={() => setShow(false)} show={showMsg} delay={4000} autohide position="top-end">
-          <Toast.Body>Woohoo, you're reading this text in a Toast!</Toast.Body>
+        <Toast onClose={() => setShowMsg(false)} show={showMsg} delay={4000} autohide>
+          <Toast.Body>{message}</Toast.Body>
         </Toast>
-      </Col>
     </Row>
   );
 }
--- src/components/pages/Booking.jsx
+++ src/components/pages/Booking.jsx
@@ -2,6 +2,7 @@ import './Booking.css'
 import AuditoriumBig from '../AuditoriumBig.jsx'
 import RangeSenior from '../RangeSenior.jsx'
 import RangeChildren from '../RangeChildren.jsx'
+import Messenger from '../Messenger.jsx'
 import Receipt from '../Receipt.jsx'
 import Button from 'react-bootstrap/Button'
 import Spinner from 'react-bootstrap/Spinner'
@@ -14,6 +15,7 @@ export default function Booking() {
 	const navigate = useNavigate()
 	const { auditoriumId, id: screeningId } = useLocation().state.screening;
 
+	const [showError, setShowError] = useState(false)
 	const [modalShow, setModalShow] = useState(false)
 	const [seatsSelected, setSeatsSelected] = useState([])
 	const [seats, setSeats] = useState([])
@@ -29,23 +31,32 @@ export default function Booking() {
 		(async () => setTickets(await getTicketTypes()))();
 	}, [])
 
+	const seatValidation = () => isSeatsValid(seatsSelected, seniors, children)
+
 	return (
 		<div className="booking">
 			<h1 className="screen-side">Screen Side</h1>
+			<Messenger showMsg={showError} setShowMsg={show => setShowError(show)} message="Please check number of selected seats and guests." />
 			{
 				(seats.length === 0 || occupiedScreenSeats.length === 0) ?
 					<Spinner animation="border" variant="warning" /> :
-					<AuditoriumBig seats={seats} occupiedScreeningSeats={occupiedScreenSeats} seatsBookClb={seats => setSeatsSelected(seats)}/>
+					<AuditoriumBig seats={seats} occupiedScreeningSeats={occupiedScreenSeats} seatsBookClb={seats => {setSeatsSelected(seats)}}/>
 			}
 			<h3>Here you can select your seats and manage discounts.</h3>
 			<RangeSenior setSeniorsClb={s => setSeniors(s)}/>
 			<hr/>
 			<RangeChildren setChildrenClb={c => setChildren(c)}/>
-			<Button bsPrefix="btn-ff" onClick={() => setModalShow(true)}>Book Tickets</Button>
+			<Button bsPrefix="btn-ff" onClick={() => {seatValidation() ? setModalShow(true) : setShowError(true)}}>Book Tickets</Button>
 			<Button bsPrefix="btn-ff" variant="secondary" onClick={() => navigate('/')}>Cancel</Button>
 			{
 				tickets.length > 0 ? <Receipt tickets={tickets} screening={occupiedScreenSeats} children={children} seniors={seniors} seats={seatsSelected} show={modalShow} onHide={() => setModalShow(false)} /> : ""
 			}
 		</div>
 	)
+}
+
+function isSeatsValid(seatsSelected, seniors, children) {
+	return (seatsSelected == 0) ? 
+		false : 
+		(seatsSelected.length - (seniors + children)) >= 0
 }
